9bc6c4c6626a0aa8d15f7901162a00e6c081a184
Hemant Arora <hemantarora1993@gmail.com>
2018-02-21 15:43:41 +0530

    First commit

--- App.js
+++ App.js
@@ -1,75 +1,61 @@
-import React from 'react';
-import { StyleSheet, Text, View, Image, ScrollView } from 'react-native';
+import React, { Component } from 'react';
+import { View, StyleSheet, Button } from 'react-native';
 
-export default class App extends React.Component {
+import t from 'tcomb-form-native';
+
+const Form = t.form.Form;
+
+const User = t.struct({
+  name: t.String,
+  lastname: t.maybe(t.String),
+  email: t.String,
+  password: t.String,
+  terms: t.Boolean
+});
+
+const options = {
+  fields: {
+    name: {
+      error: 'Name is required to move on'
+    },
+    email: {
+      error: 'Without an email address how are you going to reset your password when you forget it?'
+    },
+    password: {
+      error: 'Choose something you use on a dozen other sites or something you wont remember'
+    },
+    terms: {
+      label: 'Agree to Terms',
+    },
+  },
+};
+
+export default class App extends Component {
+  handleSubmit = () => {
+    const value = this._form.getValue();
+    console.log('value: ', value);
+  }
   render() {
     return (
-      <ScrollView>
-        <View style={styles.container}>
-          <Text style={styles.heading}>Edit Profile</Text>
-        </View>
-        <View style={{flexDirection:'column', justifyContent: 'center',alignItems: 'center', paddingTop:30, paddingBottom:20}}>
-          <Image style={{width:140, height:120, padding:50}}
-          source={{uri:'https://images.pexels.com/photos/34950/pexels-photo.jpg'}}/>
-          <Text>change profile photo</Text>
-        </View>
-        <View style={styles.common}>
-          <Text style={{fontSize: 20, color: 'white', paddingLeft:10}}>Profile Settings</Text>
-        </View>
-        <View style={styles.common2}>
-          <Text style={{fontSize:13, paddingLeft: 10}}>General information</Text>
-        </View>
-        <View style={styles.common2}>
-          <Text style={{fontSize:13, paddingLeft: 10}}>Local host information</Text>
-        </View>
-        <View style={styles.common2}>
-          <Text style={{fontSize:13, paddingLeft: 10}}>Contact & identification</Text>
-        </View>
-        <View style={styles.common}>
-          <Text style={{fontSize: 20, color: 'white', paddingLeft:10}}>Notifications</Text>
-        </View>
-        <View style={styles.common2}>
-          <Text style={{fontSize:13, paddingLeft: 10}}>Notifications Settings</Text>
-        </View>
-        <View style={styles.common}>
-          <Text style={{fontSize: 20, color: 'white', paddingLeft:10}}>Other Settings</Text>
-        </View>
-        <View style={styles.common2}>
-          <Text style={{fontSize:13, paddingLeft: 10}}>Create Password</Text>
-        </View>
-        <View style={styles.common2}>
-          <Text style={{fontSize:13, paddingLeft: 10}}>Close account</Text>
-        </View>
-        <View style={styles.common2}>
-          <Text style={{fontSize:13, paddingLeft: 10}}>Contact & support</Text>
-        </View>
-      </ScrollView>
+      <View style={styles.container}>
+        <Form
+          ref={c => this._form = c}
+          type={User}
+          options = {options} />
+        <Button
+          title="Sign Up!"
+          onPress={this.handleSubmit}
+        />
+      </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: 'red',
-    height: 70,
-    flexDirection:'row',
     justifyContent: 'center',
-  },
-  heading: {
-    color: 'white',
-    fontWeight: 'bold',
-    fontSize: 30,
-    paddingTop: 20,
-  },
-  common: {
-    backgroundColor: 'red',
-    height: 40,
-    justifyContent:'center',
-  },
-  common2: {
-    height: 40,
-    justifyContent: 'center',
-    borderBottomWidth: 1 ,
-    borderBottomColor: 'lightgrey',
+    marginTop: 50,
+    padding: 20,
+    backgroundColor: '#ffffff',
   },
 });
